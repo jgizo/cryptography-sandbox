298e5502ad6b340989136074a7d3898ff265753e
Jason <67711701+jgizo@users.noreply.github.com>
2025-04-29 23:12:50 -0400

    Delete tests directory

--- tests/test_classical.py
+++
@@ -1,8 +0,0 @@
-# test_classical.py
-def test_caesar():
-    # TODO: Write unit test for Caesar cipher
-    pass
-
-def test_vigenere():
-    # TODO: Write unit test for Vigenere cipher
-    pass
--- tests/test_modern.py
+++
@@ -1,8 +0,0 @@
-# test_modern.py
-def test_rsa():
-    # TODO: Write unit test for RSA
-    pass
-
-def test_diffie_hellman():
-    # TODO: Write unit test for Diffie-Hellman
-    pass
