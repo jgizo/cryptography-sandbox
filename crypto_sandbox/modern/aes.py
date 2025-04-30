5e93e12b033c81207e5ef348c5383f18d2e2f3ee
Jason <67711701+jgizo@users.noreply.github.com>
2025-04-29 23:11:52 -0400

    Update aes.py

--- crypto_sandbox/modern/aes.py
+++ crypto_sandbox/modern/aes.py
@@ -1,8 +1,5 @@
-# aes.py
 def encrypt(plaintext: bytes, key: bytes) -> bytes:
-    # TODO: Implement (or stub) AES encryption
     pass
 
 def decrypt(ciphertext: bytes, key: bytes) -> bytes:
-    # TODO: Implement (or stub) AES decryption
     pass
